f3a80349c575f64ee768c68cb8375047ec93c1fa
Jack Mallers <jimmymowschess@gmail.com>
2016-09-07 13:31:19 -0500

    styled header and removed frame from browser window

--- app/components/Header.js
+++ app/components/Header.js
@@ -1,9 +1,33 @@
 import React, { Component, PropTypes } from 'react';
+import { Link } from 'react-router';
+import styles from './Header.css';
 
 export default class Header extends Component {
    render() {
       return (
-         <h1>header</h1>
+         <div className={styles.headerBase}>
+            <div className={`${styles.header} ${styles.headerLeft}`}>
+               <Link to='/'>
+                  one
+               </Link>
+               <Link to='/'>
+                  two
+               </Link>
+               <Link to='/'>
+                  three
+               </Link>
+            </div>
+            <div className={`${styles.header} ${styles.headerMiddle}`}>
+               <Link to='/' className={styles.headerTitle}>
+                  bet
+               </Link>
+            </div>
+            <div className={`${styles.header} ${styles.headerRight}`}>
+               <Link to='/'>
+                  <i className="fa fa fa-search" />
+               </Link>
+            </div>
+         </div>
       )
    }
 }
